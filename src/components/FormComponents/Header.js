16287cdd16df64e6883a4601e2fe0c750b549438
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2023-01-08 21:05:40 +0530

    Refactor: Welcome Message Print

---
+++ src/components/FormComponents/Header.js
@@ -0,0 +1,21 @@
+import logo from './assets/images/logo.png'
+import './FormComponenet.css';
+
+
+function HeaderPage() {
+    return (
+        <div>
+            <header class="header-content header">
+                <div class="logo-content">
+                    <img src={logo} alt='The EmployeePayroll App Logo'/>
+                <div>
+                    <span class="emp-text">EMPLOYEE</span>
+                    <br/>
+                    <span class="emp-text emp-payroll">PAYROLL</span>
+                </div>
+                </div>
+            </header>
+        </div>
+    )
+}
+export default HeaderPage;
